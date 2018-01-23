0298a537978681cc5baff6ed1b7c06be5d50a5a8
yuzulabo <git-yuzu@svk.jp>
2018-01-23 23:00:53 +0900

    First commit

---
+++ getActivity.js
@@ -0,0 +1,18 @@
+let domain = location.search.substring(1);
+fetch("https://"+domain+"/api/v1/instance/activity", {
+    method: 'GET'
+}).then(function(response) {
+    if(response.ok) {
+        return response.json();
+    } else {
+        throw new Error();
+    }
+}).then(function(json) {
+    document.getElementById("domain").innerText = domain + "の";
+    document.getElementById("toot").innerText = json[0]["statuses"];
+    document.getElementById("login").innerText = json[0]["logins"];
+    document.getElementById("new").innerText = json[0]["registrations"];
+}).catch(function(error) {
+    document.getElementById("ok").className = "invisible";
+    document.getElementById("ng").className = "";
+});
